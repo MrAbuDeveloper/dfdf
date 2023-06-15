c1c7b4a93804098a3052da5b0ffe72f499814ed2
Abdurahmon <MrAbuDevJohnson@gmail.com>
2023-06-15 09:39:55 +0300

    dfdf

---
+++ dfdf.py
@@ -0,0 +1,60 @@
+# cars = ['cobalt', 'matiz', 'damas', 'nexia', 'malibu']
+# user_car = input('Mashinani ismini kiriting: ').lower()
+######### MAlIbu
+# print(user_car in cars)
+# if user_car in cars:
+#     print(f'Mashina topildi {cars.index(user_car) + 1}')
+# else:
+#     print(f'{user_car.capitalize()} mashinasi topilmadi')
+
+# if user_car not in cars:
+#     print('Mashina topilmadi')
+# else:
+#     print(cars.index(user_car) + 1)
+
+# cars = ['cobalt', 'matiz', 'damas', 'nexia', 'malibu']
+
+# print(cars[0])
+# print(cars[1])
+# print(cars[2])
+# print(cars[3])
+# print(cars[4])
+
+# for item in cars:
+#     print(item)
+
+# lst = [1, 's', 2, 'c', 'd', 3, 4, 'b', 'g', 'h']
+# list_for_str = []
+# list_for_int = []
+# for item in lst:
+#     if type(item) is str:
+#         list_for_str.append(item)
+#     else:
+#         list_for_int.append(item)
+# print(list_for_str)
+# print(list_for_int)
+
+
+
+# lst = [1, 2, 3, 4, 5]
+# result = 0
+# for i in lst:
+#     # result = result + i
+#     result += i
+# print(result)
+
+# user_info = "Abdurahmon Qo'chqorov 21"
+# lst = user_info.split(' ')
+# print(lst)
+
+# lst = ['Abdurahmon', 'Qo'chqorov', '21']
+# str_1 = ' '.join(lst)
+# print(str_1)
+
+### tuple
+# tpl = tuple()
+# tpl = (1, 2)
+# print(type(tpl))
+# lst = list(tpl)
+# print(lst)
+
